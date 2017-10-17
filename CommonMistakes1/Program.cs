b32b707aa7f21186a56d1416884a7d757b1447af
Hüseyin Biçen <huseynbicen@outlook.com>
2017-10-18 02:19:57 +0300

    master

--- CommonMistakes1/Program.cs
+++ CommonMistakes1/Program.cs
@@ -10,6 +10,19 @@ namespace CommonMistakes1
     {
         static void Main(string[] args)
         {
+            float num = 1f;
+            for (int i = 0; i < 10; i++)
+            {
+                num -= 0.1f;
+                Console.WriteLine(num);
+
+                if (num == 0) //Wrong!! true:  num <= 0
+                {
+                    Console.WriteLine("The programm stopped.");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
